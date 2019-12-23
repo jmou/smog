eb0898b8cdc1a511c4da390cf181ef7356bf6cc5
Joe Mou <joe@mou.fo>
2019-12-23 13:20:28 -0800

    first cut of smugmug api

---
+++ smog/api.py
@@ -0,0 +1,83 @@
+import hashlib
+import mimetypes
+import os
+
+import asks
+import oauthlib.oauth1
+import trio
+
+
+class SmugMugApi(object):
+    BASE_URI = 'https://api.smugmug.com'
+
+    def __init__(self, oauth_consumer_key, oauth_consumer_secret,
+                 oauth_token, oauth_token_secret, **oauth_kwargs):
+        self.client = oauthlib.oauth1.Client(
+            oauth_consumer_key, client_secret=oauth_consumer_secret,
+            resource_owner_key=oauth_token, resource_owner_secret=oauth_token_secret,
+            **oauth_kwargs)
+
+    async def _request_json(self, method, uri, headers=None, body=None):
+        if uri.startswith('/'):
+            uri = self.BASE_URI + uri + '?_verbosity=1'
+        if headers is None:
+            headers = {}
+        headers = {'Accept': 'application/json', 'Accept-Encoding': 'gzip', **headers}
+
+        if headers.get('Content-Type') == 'application/x-www-form-urlencoded':
+            uri, headers, body = self.client.sign(uri, http_method=method,
+                                                  headers=headers, body=body)
+        else:
+            # oauthlib barfs on non-UTF8 file upload bodies. These bodies are
+            # unsigned in OAuth anyway.
+            uri, headers, _ = self.client.sign(uri, http_method=method, headers=headers)
+
+        # TODO maybe use a session to limit concurrent connections
+        response = await asks.request(method, uri, headers=headers, data=body)
+        if 200 <= response.status_code < 300:
+            pass # TODO error handling
+        return response.json()
+
+    async def get_authuser(self):
+        return await self._request_json('GET', '/api/v2!authuser')
+
+    async def create_album_node(self, folder_node_endpoint, album_name):
+        return await self._request_json('POST', folder_node_endpoint + '!children',
+                                        headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                                        body={'Type': 'Album', 'Name': album_name, 'Privacy': 'Private'})
+
+    async def upload_image(self, album_endpoint, image_path):
+        content_type, _ = mimetypes.guess_type(image_path)
+        image_path = trio.Path(image_path)
+        body = await image_path.read_bytes()
+        headers = {
+            'Content-Length': str(len(body)),
+            'Content-Type': content_type,
+            'Content-MD5': hashlib.md5(body).hexdigest(),
+            'X-Smug-AlbumUri': album_endpoint,
+            'X-Smug-FileName': image_path.name,
+            'X-Smug-ResponseType': 'JSON',
+            'X-Smug-Version': 'v2',
+        }
+        return await self._request_json('POST', 'https://upload.smugmug.com/',
+                                        headers=headers, body=body)
+
+
+async def main():
+    oauth_consumer_key = os.environ['SMUGMUG_API_KEY']
+    oauth_consumer_secret = os.environ['SMUGMUG_API_SECRET']
+    oauth_token = os.environ['SMUGMUG_OAUTH_ACCESS_TOKEN']
+    oauth_token_secret = os.environ['SMUGMUG_OAUTH_TOKEN_SECRET']
+
+    api = SmugMugApi(oauth_consumer_key, oauth_consumer_secret,
+                     oauth_token, oauth_token_secret)
+
+    authuser_response = await api.get_authuser()
+    folder_node_endpoint = authuser_response['Response']['User']['Uris']['Node']
+    album_node_response = await api.create_album_node(folder_node_endpoint, 'test')
+    album_endpoint = album_node_response['Response']['Node']['Uris']['Album']
+    await api.upload_image(album_endpoint, 'image.png')
+
+
+if __name__ == '__main__':
+    trio.run(main)
---
+++ tests/test_api.py
@@ -0,0 +1,72 @@
+import unittest.mock
+
+import asks
+import pytest
+import trio.testing
+
+from smog.api import SmugMugApi
+
+
+# unittest.mock.AsyncMock not available until Python 3.8
+class MockRequest(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self, code=200):
+        self.args = None
+        self.kwargs = None
+        self.status_code = code
+
+    async def __call__(self, *args, **kwargs):
+        assert self.args is None
+        assert self.kwargs is None
+        self.args = args
+        self.kwargs = kwargs
+        return self
+
+    def assert_called_once_with(self, *args, **kwargs):
+        assert self.args == args
+        assert self.kwargs == kwargs
+
+    def json(self):
+        return {}
+
+
+@trio.testing.trio_test
+async def test_api(monkeypatch):
+    request = MockRequest()
+    monkeypatch.setattr(asks, 'request', request)
+    api = SmugMugApi('consumer key', 'consumer secret', 'token', 'token secret',
+                     nonce='172302863994516684631577132899', timestamp='1577132899')
+
+    assert await api.get_authuser() == {}
+    AUTH_PREFIX = 'OAuth oauth_nonce="172302863994516684631577132899", oauth_timestamp="1577132899", oauth_version="1.0", oauth_signature_method="HMAC-SHA1", oauth_consumer_key="consumer%20key", oauth_token="token", oauth_signature='
+    request.assert_called_once_with('GET', 'https://api.smugmug.com/api/v2!authuser?_verbosity=1',
+                                    headers={'Accept': 'application/json', 'Accept-Encoding': 'gzip',
+                                             'Authorization': AUTH_PREFIX + '"g3SvkYxyiqYSAsBi%2BsaC2Q%2F2H%2Bk%3D"'},
+                                    data=None)
+
+    request.reset()
+    assert await api.create_album_node('/api/v2/node/node0', 'name') == {}
+    request.assert_called_once_with('POST', 'https://api.smugmug.com/api/v2/node/node0!children?_verbosity=1',
+                                    headers={'Accept': 'application/json', 'Accept-Encoding': 'gzip',
+                                             'Authorization': AUTH_PREFIX + '"kULEPaErXQYHMY4ZRa%2FLHZ%2FW6KA%3D"',
+                                             'Content-Type': 'application/x-www-form-urlencoded'},
+                                    data={'Type': 'Album', 'Name': 'name', 'Privacy': 'Private'})
+
+    request.reset()
+    async def read_bytes(self):
+        return b'bytes'
+    monkeypatch.setattr(trio.Path, 'read_bytes', read_bytes)
+    assert await api.upload_image('/api/v2/album/album0', 'my/image.jpg') == {}
+    request.assert_called_once_with('POST', 'https://upload.smugmug.com/',
+                                    headers={'Accept': 'application/json', 'Accept-Encoding': 'gzip',
+                                             'Authorization': AUTH_PREFIX + '"VpRi1%2Bg0W7ew2UlHYN0yLdPay30%3D"',
+                                             'Content-Length': '5',
+                                             'Content-MD5': '4b3a6218bb3e3a7303e8a171a60fcf92',
+                                             'Content-Type': 'image/jpeg',
+                                             'X-Smug-AlbumUri': '/api/v2/album/album0',
+                                             'X-Smug-FileName': 'image.jpg',
+                                             'X-Smug-ResponseType': 'JSON',
+                                             'X-Smug-Version': 'v2'},
+                                    data=b'bytes')
